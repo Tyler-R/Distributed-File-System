770be40a2b56ab37f3181f804d9f4c36b18ef16f
Tyler-R <trubenui@sfu.ca>
2016-12-11 17:40:14 -0800

    Add ability to get bytes from client connection

--- src/main/java/dfs/ClientConnection.java
+++ src/main/java/dfs/ClientConnection.java
@@ -3,12 +3,15 @@ package main.java.dfs;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class ClientConnection {
-	private BufferedReader inStream = null;
-	private DataOutputStream outStream = null;
+	private InputStream inStream = null;
+	private OutputStream outStream = null;
 	
 	private Socket socket = null;
 	
@@ -16,7 +19,7 @@ public class ClientConnection {
 		this.socket = socket;
 		
 		try {
-			inStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			inStream = socket.getInputStream();
 			outStream = new DataOutputStream(socket.getOutputStream());
 		} catch (IOException e) {
 			System.out.println("ERROR creating read/write streams for client");
@@ -24,15 +27,37 @@ public class ClientConnection {
 	}
 
 	
-	public void listen() {
-		while(!socket.isClosed()) {
+	public byte[] getMessage() {
+		
+		byte buffer[] = new byte[4096];
+		int bytesRead = 0;
+		int totalBytesRead = 0;
+		
+		do {
+			bytesRead = 0;
 			try {
-				String receivedMessage = inStream.readLine();
-				outStream.writeBytes(receivedMessage);
+				bytesRead = inStream.read(buffer);
+				if(bytesRead > -1) {
+					totalBytesRead += bytesRead;
+				}
+				
+			} catch (SocketException e) {					
+				System.out.println("pipe broken");
+				try {
+					socket.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				break;
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
+				break;
 			}
-		}
+		} while(bytesRead > 0);
+		
+		byte bufferCopy[] = new byte[totalBytesRead];
+		
+		System.arraycopy(buffer, 0, bufferCopy, 0, totalBytesRead);
+		return bufferCopy;
 	}
 }
