ebc9e2a71e5300f2040288504a5d1d12246f109a
Tyler-R <trubenui@sfu.ca>
2016-12-12 04:37:49 -0800

    Add ability to read file and respond for READ message

--- src/main/java/dfs/message/request/ReadMessage.java
+++ src/main/java/dfs/message/request/ReadMessage.java
@@ -1,20 +1,46 @@
 package main.java.dfs.message.request;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigInteger;
 
+import main.java.dfs.AtomicFile;
+import main.java.dfs.ClientConnection;
 import main.java.dfs.message.Message;
 
 public class ReadMessage implements Message{
 
 	public static final String METHOD_ID = "READ";
 	private String fileName = "";
+	
+	private ClientConnection clientConnection = null;
 
-	public ReadMessage(String fileName) {
+	public ReadMessage(String fileName, ClientConnection clientConnection) {
 		this.fileName = fileName;
+		this.clientConnection = clientConnection;
 	}
 
 	@Override
 	public void execute() {	
+		
+		AtomicFile file = new AtomicFile(fileName);
+		String content = "";
+		try {
+			content = file.read();
+			try {
+				System.out.println("sending msg to client");
+				clientConnection.sendMessage(content);
+			} catch(Exception e) {
+				// there was an issue writing to the client
+				System.out.println("ERROR sending response message to client. TCP pipe is probably broken.");
+			}
+			System.out.println("file content: " + content);
+		} catch(FileNotFoundException e) {
+			System.out.println("FILE NOT FOUND: RETURN ERROR");
+		} catch(IOException e) {
+			System.out.println("IO EXCEPTION: RETURN ERROR");
+		}
+		
 		
 	} 
 	
