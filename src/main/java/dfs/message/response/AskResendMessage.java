1043725d0a0216ff1fc00e1397a18e0d4d651c70
Tyler-R <trubenui@sfu.ca>
2016-12-12 21:38:14 -0800

    Add full implementation for ASK_RESEND message

--- src/main/java/dfs/message/response/AskResendMessage.java
+++ src/main/java/dfs/message/response/AskResendMessage.java
@@ -1,18 +1,47 @@
 package main.java.dfs.message.response;
 
+import java.io.IOException;
+
+import main.java.dfs.ClientConnection;
 import main.java.dfs.message.Message;
 
 public class AskResendMessage implements Message {
 
 	public static final String METHOD_ID = "ASK_RESEND";
 	
-	public AskResendMessage(String trasactionID, String sequenceNumber) {
-		
+	private String transactionID = "";
+	private String sequenceNumber = "";
+	private ClientConnection client = null;
+	
+	public AskResendMessage(String transactionID, String sequenceNumber, ClientConnection client) {
+		this.transactionID = transactionID;
+		this.sequenceNumber = sequenceNumber;
+		this.client = client;
 	}
 	
 	@Override
 	public void execute() {
+		StringBuilder message = new StringBuilder();
 		
+		// data and errorCode can be any value is response message
+		message.append(METHOD_ID)
+				.append(" ")
+				.append(transactionID)
+				.append(" ")
+				.append(sequenceNumber)
+				.append(" ")
+				.append(ErrorCode.ASK_RESEND.getCode())
+				.append(" ")
+				.append("0") // there is no reason message so length is 0.
+				.append("\r\n\r\n\r\n");
+		
+		try {
+			client.sendMessage(message.toString());
+		} catch (IOException e) {
+			System.out.println("Could not send ASK_RESEND message for transactionID: (" 
+					+ transactionID + ") and sequenceNumber: (" + sequenceNumber 
+					+ "). IO exception \"" + e.getMessage() + "\" occured, which is likely a broken TCP pipe.");
+		}
 	}
 	
 }
--- src/main/java/dfs/message/response/ErrorCode.java
+++ src/main/java/dfs/message/response/ErrorCode.java
@@ -2,6 +2,7 @@ package main.java.dfs.message.response;
 
 public enum ErrorCode {
 	ACK ("0"),
+	ASK_RESEND ("0"),
 	INVALID_TRANSACTION_ID ("201"),
 	INVALID_OPERATION ("202"),
 	FILE_IO_ERROR ("205"),
