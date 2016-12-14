1c64031b7ed0dce6470a886771a24d60e3823a09
Tyler-R <trubenui@sfu.ca>
2016-12-13 18:50:49 -0800

    Add better error messages propogation to AtomicFile class

--- src/main/java/dfs/AtomicFile.java
+++ src/main/java/dfs/AtomicFile.java
@@ -12,20 +12,20 @@ import java.nio.channels.FileLock;
 import java.util.concurrent.locks.Lock;
 
 public class AtomicFile {
-	
+
 	private String fileName = "";
 	public static String directory = "";
-	
+
 	public static final int BUFFER_LENGTH = 4096;
-	
+
 	public AtomicFile(String fileName) {
 		this.fileName = fileName;
 	}
-	
+
 	public void append(String data) throws FileNotFoundException, IOException {
 		File file = new File(directory, fileName);
 		FileOutputStream outStream = new FileOutputStream(file, true);
-		
+
 		try {
 			FileLock lock = outStream.getChannel().lock();
 			try {
@@ -40,15 +40,15 @@ public class AtomicFile {
 		}
 
 	}
-	
+
 	public String read() throws FileNotFoundException, IOException {
 		File file = new File(directory + fileName);
 		FileInputStream inStream = new FileInputStream(file);
-		
+
 		byte buffer[] = new byte[BUFFER_LENGTH];
 		String message = "";
 		int bytesRead = 0;
-		
+
 		try {
 			FileLock lock = inStream.getChannel().lock(0L, Long.MAX_VALUE, true);
 			try {
@@ -56,31 +56,30 @@ public class AtomicFile {
 					bytesRead = 0;
 
 					bytesRead = inStream.read(buffer, 0, buffer.length);
-					if(bytesRead > -1) {
+					if (bytesRead > -1) {
 						message += new String(buffer, 0, bytesRead);
 					}
-				} while(bytesRead > 0);
-								
+				} while (bytesRead > 0);
+
 			} finally {
 				lock.release();
-			} 
-			
+			}
+
 		} catch (IOException e) {
-			String errorMessage = "Issue reading " + fileName + " because: " + (e.getMessage() != null ? e.getMessage() : "file could no be read");
-			throw(new IOException(errorMessage)); 
+			String errorMessage = "Issue reading " + fileName + " because: "
+					+ (e.getMessage() != null ? e.getMessage() : "file could no be read");
+			throw (new IOException(errorMessage));
 		} finally {
 			try {
 				inStream.close();
 			} catch (IOException e) {
-				String errorMessage = "Issue closing " + fileName + " because: " + (e.getMessage() != null ? e.getMessage() : " file could not be closed");
-				throw(new IOException(errorMessage)); 
+				String errorMessage = "Issue closing " + fileName + " because: "
+						+ (e.getMessage() != null ? e.getMessage() : " file could not be closed");
+				throw (new IOException(errorMessage));
 			}
 		}
-		
-		
-		
+
 		return message;
 	}
-	
-	
+
 }
